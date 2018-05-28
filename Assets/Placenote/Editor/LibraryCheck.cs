02d89e6895c87f604febdea5dc860929d3109183
Prasenjit Mukherjee <prashenjitmuk@gmail.com>
2018-05-28 10:23:00 -0700

    fixing librarycheck to also check file-size (mistakenly removed

--- Assets/Placenote/Editor/LibraryCheck.cs
+++ Assets/Placenote/Editor/LibraryCheck.cs
@@ -15,9 +15,11 @@ class LibraryCheck : ScriptableObject, IPreprocessBuild
 		string placenoteLibraryPath = string.Join ("/", splitPathStr.Take (splitPathStr.Length - 2).ToArray ()) + "/Plugins/iOS/Placenote.framework/Placenote"; //find library relative to this scripts path
 
 		if (!File.Exists (placenoteLibraryPath)) {
-			Debug.LogError ("Placenote library missing. Please install git lfs to download the proper file. You can follow the instructions here:\nhttps://github.com/Placenote/PlacenoteSDK-Unity");
+			Debug.LogError ("Can't find Placenote Library. It should be in Placenote/Plugins/iOS/Placenote.framework Please install git lfs to download the proper file. You can follow the instructions here:\nhttps://github.com/Placenote/PlacenoteSDK-Unity");
 		}
 
-
+		if (new FileInfo (placenoteLibraryPath).Length < 1000000) {
+			Debug.LogError ("Placenote not properly downloaded. Please install git lfs to download it properly. You can follow the instructions here:\nhttps://github.com/Placenote/PlacenoteSDK-Unity");
+		}
 	}
 }
