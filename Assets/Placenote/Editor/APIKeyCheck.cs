8e6eecf3773b508dc4e40533b2834b4f26708d05
Prasenjit Mukherjee <prashenjitmuk@gmail.com>
2018-06-20 16:21:56 -0700

    check the scene only if it has a libplacenote setup

--- Assets/Placenote/Editor/APIKeyCheck.cs
+++ Assets/Placenote/Editor/APIKeyCheck.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEditor.Build;
 using System.IO;
 
@@ -14,9 +15,10 @@ public class LibPlacenoteEditor : Editor, IPreprocessBuild
 	string filePath;
 	void OnEnable()
 	{
-		//File where APIKey gets written out
+		//File where APIKey gets written out in ever scene LibPlacenote is active
+		string sceneName = EditorSceneManager.GetActiveScene ().name;
 		filePath = Application.persistentDataPath
-			+ @"/apikey.dat";
+			+ @"/apikey_" + sceneName + ".dat";
 	}
 
 	public override void OnInspectorGUI()
@@ -31,18 +33,36 @@ public class LibPlacenoteEditor : Editor, IPreprocessBuild
 
 	public void OnPreprocessBuild(BuildTarget target, string path) {
 
-		//Right before a build starts, read the APIKey that was entered in OnInspectorGUI and make sure its not blank. 
-		StreamReader reader = new StreamReader(filePath); 
-		string keyRead = reader.ReadToEnd ();
-	
-		if (keyRead == null) {
-			Debug.LogError ("API Key Empty. Please get an API Key from http://developers.placenote.com and enter it under the LibPlacenote Object in the PlacenoteCameraManager");
-		} else if (keyRead.Trim () == "") {
-			Debug.LogError ("API Key Empty. Please get an API Key from http://developers.placenote.com and enter it under the LibPlacenote Object in the PlacenoteCameraManager");
-		} else {
-			Debug.Log ("API Key Entered:" + keyRead);
+		//Check if LibPlacenote exists, active in the current scene
+		bool libPlacenoteExists = false;
+		GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
+		foreach (GameObject go in allObjects) {
+			if (go.activeInHierarchy) {
+				if (go.GetComponent (typeof(LibPlacenote)) != null) {
+					libPlacenoteExists = true;
+				}
+			}
 		}
-		reader.Close();
+			
+		//Right before a build starts, read the APIKey that was entered in OnInspectorGUI and make sure its not blank. 
+		//If LibPlacenote does exist, try to read the file. If its empty, error out. If it doesn't exist, error out. 
+		if (libPlacenoteExists) {
+			if (File.Exists (filePath)) {
+				StreamReader reader = new StreamReader (filePath);
+				string keyRead = reader.ReadToEnd ();
+
+				if (keyRead == null) {
+					Debug.LogError ("API Key Empty. Please get an API Key from http://developers.placenote.com and enter it under the LibPlacenote Object in the PlacenoteCameraManager");
+				} else if (keyRead.Trim () == "") {
+					Debug.LogError ("API Key Empty. Please get an API Key from http://developers.placenote.com and enter it under the LibPlacenote Object in the PlacenoteCameraManager");
+				} else {
+					Debug.Log ("API Key Entered:" + keyRead);
+				}
+				reader.Close ();
+			} else {
+				Debug.LogError ("API Key Empty. Please get an API Key from http://developers.placenote.com and enter it under the LibPlacenote Object in the PlacenoteCameraManager");
+			}
+		}
 	}
 }
 #endif
