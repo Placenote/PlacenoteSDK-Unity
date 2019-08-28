d68f440206d973fb2f7c2feb3c9a534615271a0e
Yan Ma <yan@vertical.ai>
2019-08-28 14:28:32 -0400

    added manual thumbnail selection

--- Assets/Placenote/LibPlacenote/LocalizationThumbnailSelector.cs
+++ Assets/Placenote/LibPlacenote/LocalizationThumbnailSelector.cs
@@ -14,9 +14,11 @@ public class LocalizationThumbnailSelector : MonoBehaviour, PlacenoteListener
     private int mMaxLmSize = -1;
     private RenderTexture mBestRenderTexture;
     private Texture2D mThumbnailTexture;
+    private bool mThumbnailSelected = false;
 
     [SerializeField] int mThumbnailScale = 6;
     [SerializeField] RawImage mImage;
+    [SerializeField] Text mLabelText;
     [SerializeField] ARCameraBackground mARBackground;
 
     public static LocalizationThumbnailSelector Instance
@@ -49,6 +51,12 @@ public class LocalizationThumbnailSelector : MonoBehaviour, PlacenoteListener
 
     public void OnPose(Matrix4x4 outputPose, Matrix4x4 arkitPose)
     {
+        // if it's been manually selected, don't keep on auto-selecting
+        if (mThumbnailSelected)
+        {
+            return;
+        }
+
         if (LibPlacenote.Instance.GetMode() != LibPlacenote.MappingMode.MAPPING)
         {
             return;
@@ -72,19 +80,61 @@ public class LocalizationThumbnailSelector : MonoBehaviour, PlacenoteListener
         if (lmSize > mMaxLmSize)
         {
             mMaxLmSize = lmSize;
-            if (mImage != null && Screen.width / mThumbnailScale != (int)mImage.rectTransform.rect.width)
-            {
-                mImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
-                    Screen.width / mThumbnailScale);
-                mImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
-                    Screen.height / mThumbnailScale);
-                mImage.rectTransform.ForceUpdateRectTransforms();
-            }
-            Graphics.Blit(null, mBestRenderTexture, mARBackground.material);
-            RenderTexture.active = mBestRenderTexture;
-            mThumbnailTexture.ReadPixels(new Rect(0, 0, mBestRenderTexture.width, mBestRenderTexture.height), 0, 0);
-            mThumbnailTexture.Apply();
+            Debug.Log("Updated thumbnail with " + mMaxLmSize + " tracked landmarks");
+            SetCurrentImageAsThumbnail();
         }
+    }
+
+    private void SetCurrentImageAsThumbnail()
+    {
+        if (mImage != null && Screen.width / mThumbnailScale != (int)mImage.rectTransform.rect.width)
+        {
+            mImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,
+                Screen.width / mThumbnailScale);
+            mImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,
+                Screen.height / mThumbnailScale);
+            mImage.rectTransform.ForceUpdateRectTransforms();
+        }
+        Graphics.Blit(null, mBestRenderTexture, mARBackground.material);
+        RenderTexture.active = mBestRenderTexture;
+        mThumbnailTexture.ReadPixels(new Rect(0, 0, mBestRenderTexture.width, mBestRenderTexture.height), 0, 0);
+        mThumbnailTexture.Apply();
+    }
+
+    public void OnSelectThumbnailClick()
+    {
+        if (LibPlacenote.Instance.GetMode() != LibPlacenote.MappingMode.MAPPING)
+        {
+            // Prompt that it's not in mapping mode
+            return;
+        }
+
+        LibPlacenote.PNFeaturePointUnity[] trackedLandmarks = LibPlacenote.Instance.GetTrackedFeatures();
+        if (trackedLandmarks == null)
+        {
+            // Prompt that there's not enough features
+            return;
+        }
+
+        int lmSize = 0;
+        for (int i = 0; i < trackedLandmarks.Length; i++)
+        {
+            if (trackedLandmarks[i].measCount > 4)
+            {
+                lmSize++;
+            }
+        }
+
+        if (lmSize < 20)
+        {
+            // Prompt that there's not enough features
+            mLabelText.text = "Can't select thumbnail with " + lmSize + " < 20 landmarks";
+            return;
+        }
+
+        mThumbnailSelected = true;
+        SetCurrentImageAsThumbnail();
+        mLabelText.text = "Selected new thumbnail!";
     }
 
     public void OnStatusChange(LibPlacenote.MappingStatus prevStatus, LibPlacenote.MappingStatus currStatus)
@@ -92,6 +142,7 @@ public class LocalizationThumbnailSelector : MonoBehaviour, PlacenoteListener
         if (prevStatus != LibPlacenote.MappingStatus.WAITING && currStatus == LibPlacenote.MappingStatus.WAITING)
         {
             mMaxLmSize = -1;
+            mThumbnailSelected = false;
             mImage.gameObject.SetActive(false);
         }
         else if (prevStatus == LibPlacenote.MappingStatus.WAITING)
